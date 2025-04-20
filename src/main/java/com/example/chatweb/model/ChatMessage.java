990e8dc7f41906d03ff0fd56066a71ab79b06b18
PigCassoKien <kien0610minh@gmail.com>
2025-04-21 00:46:21 +0700

    Test webSocket

---
+++ src/main/java/com/example/chatweb/model/ChatMessage.java
@@ -0,0 +1,15 @@
+package com.example.chatweb.model;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class ChatMessage {
+
+    private String content;
+    private String sender;
+    private MessageType type;
+}
---
+++ src/main/java/com/example/chatweb/model/MessageType.java
@@ -0,0 +1,8 @@
+package com.example.chatweb.model;
+
+public enum MessageType {
+
+    CHAT,
+    JOIN,
+    LEAVE
+}
